9cd8e085b0c4042d333ef53dee7c631c080682a8
enarsinick <nxdmedia@googlemail.com>
2021-11-08 17:29:36 +0000

    Amended String Manipulation file

--- src/com/qa/qacommunity/StringManipulation2.java
+++ src/com/qa/qacommunity/StringManipulation2.java
@@ -4,43 +4,71 @@ public class StringManipulation2 {
 
 	public static void main(String[] args) {
 
-		System.out.println(countWords("Hello"));
-		verticalString("Hello");
-		verticalStringReverse("Goodbye");
-		findWord("That's the way the cookie crumbles", "cookie");
+//		System.out.println(countWords("That's the way the cookie crumbles"));
+//		verticalString("That's the way the cookie crumbles");
+//		verticalStringReverse("That's the way the cookie crumbles");
+		System.out.println(findWord("That's the way the cookie crumbles", "cookie"));
 	}
 	
 	// When given a String, count and return 
 	// how many words there are in that String.
-	public static int countWords(String word) {
-		return word.length();
+	public static int countWords(String message) {
+		int numOfWords = 0;
+		
+		for (int i = 0; i < message.length(); i++) {
+			if (message.substring(i, i + 1).equals(" ")) numOfWords++;
+		}
+		
+		return numOfWords + 1;
 	}
 	
-	public static void verticalString(String word) {
-		for (int i = 1; i <= word.length(); i++ ) {
-			System.out.println(word.substring(i - 1, i));
+	public static void verticalString(String message) {
+		String word = "";
+		String letter;
+		
+		for (int i = 0; i < message.length(); i++) {
+			letter = message.substring(i, i + 1);
+			if (!letter.equals(" ")) {
+				word += letter ;
+			} else {
+				System.out.println(word);
+				word = "";
+			}
 		}
 	}
 	
-	public static void verticalStringReverse(String word) {
-		for (int i = word.length(); i >= 1; i--) {
-			System.out.println(word.substring(i - 1, i));
+	public static void verticalStringReverse(String message) {
+		String word = "";
+		String letter;
+		
+		for (int i = message.length(); i >= 1; i--) {
+			letter = message.substring(i - 1, i);
+			if (!letter.equals(" ")) {
+				word += letter ;
+			} else {
+				System.out.println(word);
+				word = "";
+			}
 		}
 	}
 	
-//	public static boolean findWord(String message, String word) {
-//		
-//		int start = 0;
-//		int end = word.length();
-//		
-//		for (int i = 0; i < message.length(); i++) {
-//			if (message.substring(start, end).equals(word)) {
-//				return true;
-//			} else {
-//				start += end;
-//				end += end;
-//			}
-//		}
-//	}
+	public static boolean findWord(String message, String search) {
+		
+		String word = "";
+		String letter;
+		
+		for (int i = 0; i < message.length(); i++) {
+			letter = message.substring(i, i + 1);
+			if (!letter.equals(" ")) {
+				word += letter ;
+			} else {
+				if (word.equals(search)) {
+					return true;
+				}
+				word = "";
+			}
+		}
+		return false;
+	}
 
 }
